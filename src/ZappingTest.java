2cd44d08a1615aca980231b4a9fdd3bd5867adef
Kikoromero97 <kikoromero1997@gmail.com>
2023-05-11 19:10:35 +0200

    Segundo commit - Tasca 1 Zapping

--- src/ZappingTest.java
+++ src/ZappingTest.java
@@ -7,4 +7,10 @@ class ZappingTest {
         int resultadoEsperado = Zapping.numeroClicks(0, 0);
         assertEquals(0, resultadoEsperado);
     }
+
+    @org.junit.jupiter.api.Test
+    void numeroClicks2() {
+        int resultadoEsperado = Zapping.numeroClicks(1, 2);
+        assertEquals(1, resultadoEsperado);
+    }
 }
