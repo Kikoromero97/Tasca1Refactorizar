1431512c184b3a9cf8dfbfbcc7b5a67934b753e4
Kikoromero97 <kikoromero1997@gmail.com>
2023-05-11 19:28:20 +0200

    Tercer commit - Tasca 1 Zapping

--- src/Zapping.java
+++ src/Zapping.java
@@ -1,17 +1,15 @@
 public class Zapping {
-    public static int numeroClicks (int canalOrigen, int canalDestino) {
+    public static int numeroClicks(int canalOrigen, int canalDestino) {
         int clicks = 0;
-        do {
+        while (canalOrigen != canalDestino) {
             clicks++;
             if (canalDestino > canalOrigen) {
                 canalOrigen = (canalOrigen + 1) % 100;
-            } else if (canalDestino < canalOrigen) {
-                canalOrigen = (canalOrigen + 99) % 100;
             } else {
-                clicks = 0;
-                break;
+                canalOrigen = (canalOrigen + 99) % 100;
             }
-        } while (canalOrigen != canalDestino);
+        }
         return clicks;
     }
 }
+
--- src/ZappingTest.java
+++ src/ZappingTest.java
@@ -13,4 +13,14 @@ class ZappingTest {
         int resultadoEsperado = Zapping.numeroClicks(1, 2);
         assertEquals(1, resultadoEsperado);
     }
+    @org.junit.jupiter.api.Test
+    void numeroClicks3() {
+        int resultadoEsperado = Zapping.numeroClicks(10, 30);
+        assertEquals(20, resultadoEsperado);
+    }
+    @org.junit.jupiter.api.Test
+    void numeroClicks4() {
+        int resultadoEsperado = Zapping.numeroClicks(2, 99);
+        assertEquals(2, resultadoEsperado);
+    }
 }
